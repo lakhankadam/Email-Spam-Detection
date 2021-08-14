22a634371382349f4d11b46b2659d68eacb5abba
lakhankadam1 <lakhankadam1@gmail.com>
2021-08-14 19:07:41 +0530

    Adding evaluate model to test training and test data

---
+++ evaluate_model.py
@@ -0,0 +1,21 @@
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from train_model import train_model
+from split_data import split_train_test_data
+
+def test_training_data():
+    classifier = train_model()
+    xtrain, xtest, ytrain, ytest = split_train_test_data()
+    pred = classifier.predict(xtrain)
+    print(classification_report(ytrain, pred))
+    print()
+    print("Confusion Matrix: \n", confusion_matrix(ytrain, pred))
+    print("Accuracy: \n", accuracy_score(ytrain, pred))
+
+def test_test_data():
+    classifier = train_model()
+    xtrain, xtest, ytrain, ytest = split_train_test_data()
+    pred = classifier.predict(xtest)
+    print(classification_report(ytest, pred))
+    print()
+    print("Confusion Matrix: \n", confusion_matrix(ytest, pred))
+    print("Accuracy: \n", accuracy_score(ytest, pred))
